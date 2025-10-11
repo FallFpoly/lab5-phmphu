d53f39eb2dab0ae702b8af7cfb7f75b6f3c26b4b
Admin <Admin@172.16.121.131>
2025-10-11 09:05:33 +0700

    nộp lab2 Signed-off-by:Admin <Admin@192.168.100.225> Signed-off-by:Admin <Admin@192.168.100.225> Signed-off-by:Admin <Admin@172.16.121.131>

--- Lab5/src/main/java/com/mycompany/lab5/bai1.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai1.java
@@ -1,16 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+//Name : 
+//Mssv : 
+//Date : 
+//Slide:
 
 package com.mycompany.lab5;
 
-/**
- *
- * @author ICT
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        //
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> ds = new ArrayList<>();
+        double x;
+                int i=0;
+        while(true){
+            System.out.println("nhap  pt thu "+i+": ");
+            System.out.print("nhap x:");
+            x = sc.nextDouble();
+            ds.add(x);
+           sc.nextLine();
+            System.out.println("nhap them (Y/N)?");
+            String tiepTuc = sc.nextLine();
+           // sc.close();
+            if(tiepTuc.equalsIgnoreCase("N")){
+                break;
+            }
+            i++;
+            
+        }
+        System.out.println("danh sach so thuc: ");
+        double tong = 0;
+        for(Double so: ds){
+            System.out.println("so"+ds);
+            tong+=so;
+            
+        }
+        System.out.println("tong cac so thuc: "+tong);
     }
+
 }
